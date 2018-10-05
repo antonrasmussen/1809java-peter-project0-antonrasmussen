244447f5c820646a860a0fe399ea8f50653da9fa
arasmuss <arasm002@odu.edu>
2018-10-05 08:17:07 -0400

    Update interface methods

--- src/main/java/com/revature/service/BankService.java
+++ src/main/java/com/revature/service/BankService.java
@@ -1,5 +1,10 @@
 package com.revature.service;
 
+import java.util.Set;
+
+import com.revature.model.Account;
+import com.revature.model.Customer;
+
 /**
  * The BankService class provides functionality for authentication, 
  * validation, and registration.
@@ -11,8 +16,18 @@ package com.revature.service;
 
 public interface BankService {
 	
+	// Account Stuff
+	boolean registerAccount(Account account);
+	boolean registerAccountSecure(Account account);
+	Set<Account> getAllAccounts();
+	Set<Account> getAccount(String accountType);
+	Account getAccount(Long accountNumber);
+	
+	// Customer Stuff
 	boolean registerCustomer(Customer customer);
-	
-	
+	boolean registerCustomerSecure(Customer customer);
+	Set<Customer> getAllCustomers();
+	Set<Customer> getCustomer(String firstName, String lastName);
+	Customer getCustomer(Long id);	
 
 }
