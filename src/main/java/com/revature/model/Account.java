905a8b37e1716958e9cb0b4e3e16f636e195ffaf
arasmuss <arasm002@odu.edu>
2018-10-05 23:32:56 -0400

    reorg fields according to DB schema

--- src/main/java/com/revature/model/Account.java
+++ src/main/java/com/revature/model/Account.java
@@ -3,7 +3,7 @@ package com.revature.model;
 import java.io.Serializable;
 
 public class Account implements Comparable<Account>, Serializable {
-	
+
 	/**
 	 * Java 1.x Compatibility
 	 */
@@ -13,34 +13,45 @@ public class Account implements Comparable<Account>, Serializable {
 	 * A_ACCOUNT_NUMBER (PK) - (Must be unique + not nullable)
 	 */
 	private Long accountNumber;
-	
+
 	/*
 	 * A_ACCOUNT_TYPE - (Not null) - 'CHECKING', 'SAVINGS', 'CUSTODIAL', 'IRA'
 	 */	
 	private String accountType;
-	
+
 	/*
 	 * A_ACCOUNT_STATUS - (Can be null) - 'ACTIVE', 'INACTIVE'
 	 */
 	private String accountStatus;
-	
+
 	/*
 	 * A_ACCOUNT_BALANCE - (Not null)
 	 */
 	private double accountBalance;
 	
-	
+	/*
+	 * 	ACCOUNT_HASH
+	 */
+	private String accountHash;
+
+
 	private Customer customer;
-	
+
 	public Account() {}
 
-	public Account(Long accountNumber, String accountType, String accountStatus, double accountBalance, Customer customer) {
-		
+	public Account(Long accountNumber, 
+			String accountType, 
+			String accountStatus,
+			double accountBalance,
+			Customer customer,
+			String accountHash) {
+
 		this.accountNumber = accountNumber;
 		this.accountType = accountType;
 		this.accountStatus = accountStatus;
 		this.accountBalance = accountBalance;
 		this.customer = customer;
+		this.accountHash = accountHash;
 	}
 
 
@@ -75,14 +86,23 @@ public class Account implements Comparable<Account>, Serializable {
 	public void setAccountBalance(double accountBalance) {
 		this.accountBalance = accountBalance;
 	}
+	
+	public String getAccountHash() {
+		return accountHash;
+	}
+
+	public void setAccountHash(String accountHash) {
+		this.accountHash = accountHash;
+	}
 
 	public Customer getCustomer() {
 		return customer;
 	}
-	
+
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
+
 
 	@Override
 	public int hashCode() {
@@ -90,10 +110,12 @@ public class Account implements Comparable<Account>, Serializable {
 		int result = 1;
 		long temp;
 		temp = Double.doubleToLongBits(accountBalance);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + (int) (temp ^ (temp >>> 32));		
 		result = prime * result + ((accountNumber == null) ? 0 : accountNumber.hashCode());
 		result = prime * result + ((accountStatus == null) ? 0 : accountStatus.hashCode());
 		result = prime * result + ((accountType == null) ? 0 : accountType.hashCode());
+		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + ((accountHash == null) ? 0 : accountHash.hashCode());
 		return result;
 	}
 
@@ -123,19 +145,31 @@ public class Account implements Comparable<Account>, Serializable {
 				return false;
 		} else if (!accountType.equals(other.accountType))
 			return false;
+		if (customer == null) {
+			if (other.customer != null)
+				return false;
+		} 
+		if (accountHash == null) {
+			if (other.accountHash != null)
+				return false;
+		} else if (!accountHash.equals(other.accountHash))
+			return false;
+		else if (!customer.equals(other.customer))
+			return false;
 		return true;
 	}
 
-	
+
 	@Override
 	public String toString() {
 		return "Account [accountNumber=" + accountNumber + ", accountType=" + accountType + ", accountStatus="
-				+ accountStatus + ", accountBalance=" + accountBalance + "]";
+				+ accountStatus + ", accountBalance=" + accountBalance + ", accountHash=" + accountHash + ", customer="
+				+ customer + "]";
 	}
 
 	@Override
 	public int compareTo(Account account) {
-		
+
 		return new Long(this.accountNumber).compareTo(account.accountNumber);
 	}
 
