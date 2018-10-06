94e3d222cc2847134e358a2798f95c6504772c6e
arasmuss <arasm002@odu.edu>
2018-10-05 23:38:36 -0400

    Begin registration exception class

---
+++ src/main/java/com/revature/exception/BankRegistrationException.java
@@ -0,0 +1,16 @@
+package com.revature.exception;
+
+public class BankRegistrationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3706430178014220784L;
+	
+	public BankRegistrationException(String message) {
+		super(message);
+	}
+	
+	
+
+}
